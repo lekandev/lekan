9e18fa1cc68e109cc5f82fc01cc4c69ec98043a6
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-03-06 13:24:20 +0100

    📱 Mobile version and content update

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
 console.log("Lekan is Live");
+
+const contact = document.getElementById("contact__details");
+const projectName = document.getElementsByClassName("project__title");
+const projectNames = ["Airbnb Clone", "Netflix Clone"];
+
+// contact.innerHTML = "<h1>Contact Form is not ready yet!!</h1> <button id='view'>Check Progress</button>";
+
+const viewContactForm = document.getElementById("view");
+
+viewContactForm.addEventListener("click", () => {})
+
+projectNames.map(project => {
+    projectName.innerHTML 
+})
