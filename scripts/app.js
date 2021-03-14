ea7f3e75a1fda370f6de55df18d6524a54c95042
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-03-15 00:55:46 +0100

    💖 Site is looking goooood!!!

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,20 @@
 console.log("Lekan is Live");
+const sideNav = document.querySelector(".res");
+const submitBtn = document.querySelector(".submit");
+const thankYou = document.querySelector(".thank-you-msg");
+
+sideNav.addEventListener("click", () => {
+    console.log("yay, it works!!");
+});
+
+submitBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    thankYou.innerHTML = "😁 Thank You, I'll get back to you shortly!";
+})
 
 let animatedBox = anime({
-    targets: '',
-    translateY: 100,
-    duration: 1000,
-    easing: linear
+  targets: ".square",
+  translateY: 100,
+  duration: 1000,
+  easing: linear,
 });
